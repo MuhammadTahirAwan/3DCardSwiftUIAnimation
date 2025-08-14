288543b93c9aeb3fd3d2bc9b9aa92b6526665b83
Tahir Awan <tahirawan94@icloud.com>
2025-08-13 21:16:06 -0300

    Initial Commit

---
+++ 3DCardSwiftUIAnimation/_DCardSwiftUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DCardSwiftUIAnimationApp.swift
+//  3DCardSwiftUIAnimation
+//
+//  Created by Tahir Awan on 13/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct _DCardSwiftUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 3DCardSwiftUIAnimationTests/_DCardSwiftUIAnimationTests.swift
@@ -0,0 +1,17 @@
+//
+//  _DCardSwiftUIAnimationTests.swift
+//  3DCardSwiftUIAnimationTests
+//
+//  Created by Tahir Awan on 13/08/2025.
+//
+
+import Testing
+@testable import _DCardSwiftUIAnimation
+
+struct _DCardSwiftUIAnimationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
